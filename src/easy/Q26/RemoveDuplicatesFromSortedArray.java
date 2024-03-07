117d2bd9d7f9cd9c375088b8663c188f1d308a24
tzhau <u10316035@go.utaipei.edu.tw>
2024-03-07 23:24:43 +0800

    Q26

--- src/easy/Q26/RemoveDuplicatesFromSortedArray.java
+++ src/easy/Q26/RemoveDuplicatesFromSortedArray.java
@@ -1,4 +1,32 @@
 package easy.Q26;
 
+class Solution {
+    public static int removeDuplicates(int[] nums) {
+        int currentIdx = 0;
+        int currentNum = -101;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>currentNum){
+                currentNum = nums[i];
+                int temp = nums[i];
+                nums[currentIdx] = currentNum;
+                nums[i] = temp;
+                currentIdx++;
+            }
+        }
+
+        int count = 1;
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]<nums[i+1]){
+                count++;
+            }else{
+                break;
+            }
+        }
+        return count;
+    }
+}
 public class RemoveDuplicatesFromSortedArray {
+    public static void main(String[] args) {
+        System.out.println(Solution.removeDuplicates(new int[]{1, 1, 2, 3, 4}));
+    }
 }
